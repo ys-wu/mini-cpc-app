078d673f4e7a50ed2e95ae20fffb7d0164ecd213
Yusheng Wu <im.yushengwu@gmail.com>
2023-01-03 17:24:25 +0200

    find RPi through ethernet

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # mini-cpc-app
+
+## Raspberry Pi
+
+### Environment
+
+- Operating System: Raspberry Pi OS (32-bit), Realeased: 2022-09-22
+- `Python --version`: 3.9.2
+
+### Setup
+
+- Enable SSH
---
+++ backend/utils.py
@@ -0,0 +1,18 @@
+import subprocess
+import re
+
+
+def get_ethernet_rpi_ips():
+    result = subprocess.run(['arp', '-a'], stdout=subprocess.PIPE)
+    arp_entries = result.stdout.decode('utf-8').split('\n')
+    rpi_entries = [entry for entry in arp_entries if "raspberrypi" in entry]
+
+    ip_regex = r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b"
+    rpi_ips = [re.search(ip_regex, entry).group(0) for entry in rpi_entries]
+
+    return rpi_ips
+
+
+if __name__ == "__main__":
+    rpi_ips = get_ethernet_rpi_ips()
+    print(rpi_ips)
